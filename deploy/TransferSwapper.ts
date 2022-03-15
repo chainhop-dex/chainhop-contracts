1c2ff7c277a7672ad4d985fffa107fa066d542b4
padoriku <patrickmao@live.com>
2022-03-15 17:03:41 +0800

    deploy

--- deploy/TransferSwapper.ts
+++ deploy/TransferSwapper.ts
@@ -9,23 +9,24 @@ const deployFunc: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  await deploy('UniswapV2SwapExactTokensForTokensCodec', {
-    from: deployer,
-    log: true
-  });
+  await deploy('UniswapV2SwapExactTokensForTokensCodec', { from: deployer, log: true });
   const v2Codec = await deployments.get('UniswapV2SwapExactTokensForTokensCodec');
 
-  await deploy('TransferSwapper', {
-    from: deployer,
-    log: true,
-    args: [
-      process.env.MESSAGE_BUS,
-      process.env.NATIVE_WRAP,
-      process.env.FEE_SIGNER,
-      process.env.FEE_COLLECTOR,
-      ['swapExactTokensForTokens(uint256,uint256,address[],address,uint256)'],
-      [v2Codec.address]
-    ]
+  const args = [
+    process.env.MESSAGE_BUS,
+    process.env.NATIVE_WRAP,
+    process.env.FEE_SIGNER,
+    process.env.FEE_COLLECTOR,
+    ['swapExactTokensForTokens(uint256,uint256,address[],address,uint256)'],
+    [v2Codec.address]
+  ];
+
+  await deploy('TransferSwapper', { from: deployer, log: true, args });
+  const xswap = await deployments.get('TransferSwapper');
+
+  await hre.run('verify:verify', {
+    address: xswap.address,
+    constructorArguments: args
   });
 };
 
