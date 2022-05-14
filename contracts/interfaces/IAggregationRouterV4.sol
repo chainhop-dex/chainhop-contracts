f07e79beb4a56501c8af2a92cf5811ba3ef3af2a
padoriku <patrickmao@live.com>
2022-05-14 19:26:22 +0800

    add 1inch aggregation router interface

---
+++ contracts/interfaces/IAggregationRouterV4.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+pragma solidity >=0.8.12;
+
+// 1inch's AggregationRouterV4
+interface IAggregationRouterV4 {
+    // uniswap v3 swapper method
+    function uniswapV3Swap(
+        uint256 amount,
+        uint256 minReturn,
+        uint256[] calldata pools
+    ) external payable returns (uint256 returnAmount);
+}
