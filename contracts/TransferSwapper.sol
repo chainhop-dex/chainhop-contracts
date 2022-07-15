f4e6c50d1478e24d5b51819bf57b06aaeb614a73
padoriku <patrickmao@live.com>
2022-07-15 18:52:26 +0800

    Revert "skip fee verification if the swap is direct bridge"
    This reverts commit 1b030dc20fe28d1c35d2045ef4cadc287ab0dcd6.

--- contracts/TransferSwapper.sol
+++ contracts/TransferSwapper.sol
@@ -197,14 +197,10 @@ contract TransferSwapper is MessageReceiverApp, Swapper, SigVerifier, FeeOperato
         address bridgeOutReceiver = _dstSwaps.length > 0 ? _desc.dstTransferSwapper : _desc.receiver;
         bytes memory bridgeResp;
         {
-            uint256 msgFee = 0;
-            if (_dstSwaps.length > 0) {
-                _verifyFee(_desc, _amountIn, srcToken);
-                if (_desc.nativeIn) {
-                    msgFee = msg.value - _amountIn;
-                } else {
-                    msgFee = msg.value;
-                }
+            _verifyFee(_desc, _amountIn, srcToken);
+            uint256 msgFee = msg.value;
+            if (_desc.nativeIn) {
+                msgFee = msg.value - _amountIn;
             }
             IBridgeAdapter bridge = bridges[keccak256(bytes(_desc.bridgeProvider))];
             IERC20(bridgeToken).safeIncreaseAllowance(address(bridge), _amountOut);
