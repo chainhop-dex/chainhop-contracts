edd48248d63c5bb018c23772c496c4dee63ff9a1
padoriku <patrickmao@live.com>
2022-09-05 17:28:25 +0800

    refine config

--- deploy/configs/config.ts
+++ deploy/configs/config.ts
@@ -1,12 +1,13 @@
 import * as dotenv from 'dotenv';
-import { getMetaPoolCodecConfig, getSpecialMetaPoolCodecConfig, getSupportedCurvePools } from './functions';
+import {
+  getMetaPoolCodecConfig,
+  getSpecialMetaPoolCodecConfig,
+  getSupportedCurvePools,
+  getSupportedOneInchFuncs
+} from './functions';
 import {
   CurvePoolCodec,
   IConfig,
-  OneInchClipperSwapFunc,
-  OneInchSwapFunc,
-  OneInchUnoswapSwapFunc,
-  OneInchUnoswapV3SwapFunc,
   PlatypusRouter01Codec,
   UniswapV2SwapExactTokensForTokensCodec,
   UniswapV3ExactInputCodec
@@ -25,11 +26,7 @@ export const deploymentConfigs: IConfig = {
     supportedDex: [
       { address: '0xE592427A0AEce92De3Edee1F18E0157C05861564', func: UniswapV3ExactInputCodec.func }, // UniswapV3: SwapRouter
       { address: '0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F', func: UniswapV2SwapExactTokensForTokensCodec.func }, // SushiSwap: SushiSwapRouter
-      // 1inch address and funcs
-      { address: '0x1111111254fb6c44bAC0beD2854e76F90643097d', func: OneInchSwapFunc },
-      { address: '0x1111111254fb6c44bAC0beD2854e76F90643097d', func: OneInchClipperSwapFunc },
-      { address: '0x1111111254fb6c44bAC0beD2854e76F90643097d', func: OneInchUnoswapSwapFunc },
-      { address: '0x1111111254fb6c44bAC0beD2854e76F90643097d', func: OneInchUnoswapV3SwapFunc },
+      ...getSupportedOneInchFuncs('0x1111111254fb6c44bAC0beD2854e76F90643097d'),
       ...getSupportedCurvePools(1)
     ],
     codecs: [
@@ -40,9 +37,8 @@ export const deploymentConfigs: IConfig = {
       getMetaPoolCodecConfig(1)
     ],
     externalSwapDex: ['0x1111111254fb6c44bAC0beD2854e76F90643097d'], // 1inch
-    transferSwapper: process.env.TRANSFER_SWAPPER_1, //set with the actual addr after TransferSwapper is deployed
-    anyswapRouters: (process.env.ANYSWAP_ROUTER_1 ?? '').split(','),
-    stargateRouters: (process.env.STARGATE_ROUTER_1 ?? '').split(',')
+    anyswapRouters: ['0x6b7a87899490ece95443e979ca9485cbe7e71522', '0x765277eebeca2e31912c9946eae1021199b39c61'],
+    stargateRouters: []
   },
 
   // Optimism
@@ -51,17 +47,12 @@ export const deploymentConfigs: IConfig = {
     messageBus: process.env.MESSAGE_BUS_10,
     supportedDex: [
       { address: '0xE592427A0AEce92De3Edee1F18E0157C05861564', func: UniswapV3ExactInputCodec.func }, // UniswapV3: SwapRouter
-      // 1inch address and funcs
-      { address: '0x1111111254760f7ab3f16433eea9304126dcd199', func: OneInchSwapFunc },
-      { address: '0x1111111254760f7ab3f16433eea9304126dcd199', func: OneInchClipperSwapFunc },
-      { address: '0x1111111254760f7ab3f16433eea9304126dcd199', func: OneInchUnoswapSwapFunc },
-      { address: '0x1111111254760f7ab3f16433eea9304126dcd199', func: OneInchUnoswapV3SwapFunc }
+      ...getSupportedOneInchFuncs('0x1111111254760f7ab3f16433eea9304126dcd199')
     ],
     codecs: [UniswapV3ExactInputCodec],
     externalSwapDex: ['0x1111111254760f7ab3f16433eea9304126dcd199'], // 1inch
-    transferSwapper: process.env.TRANSFER_SWAPPER_10, //set with the actual addr after TransferSwapper is deployed
-    anyswapRouters: (process.env.ANYSWAP_ROUTER_10 ?? '').split(','),
-    stargateRouters: (process.env.STARGATE_ROUTER_10 ?? '').split(',')
+    anyswapRouters: ['0xDC42728B0eA910349ed3c6e1c9Dc06b5FB591f98', '0x80a16016cc4a2e6a2caca8a4a498b1699ff0f844'],
+    stargateRouters: ['0xB0D502E938ed5f4df2E681fE6E419ff29631d62b', '0xB49c4e680174E331CB0A7fF3Ab58afC9738d5F8b']
   },
 
   // BSC
@@ -71,32 +62,23 @@ export const deploymentConfigs: IConfig = {
     supportedDex: [
       { address: '0x10ed43c718714eb63d5aa57b78b54704e256024e', func: UniswapV2SwapExactTokensForTokensCodec.func }, // PancakeSwap: UniswapV2Router02
       { address: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506', func: UniswapV2SwapExactTokensForTokensCodec.func }, // SushiSwap: SushiSwapRouter
-      // 1inch address and funcs
-      { address: '0x1111111254fb6c44bAC0beD2854e76F90643097d', func: OneInchSwapFunc },
-      { address: '0x1111111254fb6c44bAC0beD2854e76F90643097d', func: OneInchClipperSwapFunc },
-      { address: '0x1111111254fb6c44bAC0beD2854e76F90643097d', func: OneInchUnoswapSwapFunc },
-      { address: '0x1111111254fb6c44bAC0beD2854e76F90643097d', func: OneInchUnoswapV3SwapFunc }
+      ...getSupportedOneInchFuncs('0x1111111254fb6c44bAC0beD2854e76F90643097d')
     ],
     codecs: [UniswapV2SwapExactTokensForTokensCodec],
     externalSwapDex: ['0x1111111254fb6c44bac0bed2854e76f90643097d'], // 1inch
-    transferSwapper: process.env.TRANSFER_SWAPPER_56, //set with the actual addr after TransferSwapper is deployed
-    anyswapRouters: (process.env.ANYSWAP_ROUTER_56 ?? '').split(','),
-    stargateRouters: (process.env.STARGATE_ROUTER_56 ?? '').split(',')
+    anyswapRouters: ['0xd1c5966f9f5ee6881ff6b261bbeda45972b1b5f3', '0xabd380327fe66724ffda91a87c772fb8d00be488'],
+    stargateRouters: ['0x4a364f8c717cAAD9A442737Eb7b8A55cc6cf18D8']
   },
 
   // Polygon
   137: {
-    nativeWrap: '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270',
+    nativeWrap: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
     messageBus: process.env.MESSAGE_BUS_137,
     supportedDex: [
       { address: '0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff', func: UniswapV2SwapExactTokensForTokensCodec.func }, // Quickswap: UniswapV2Router02
       { address: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506', func: UniswapV2SwapExactTokensForTokensCodec.func }, // SushiSwap: SushiSwapRouter
       { address: '0xE592427A0AEce92De3Edee1F18E0157C05861564', func: UniswapV3ExactInputCodec.func }, // UniswapV3: SwapRouter
-      // 1inch address and funcs
-      { address: '0x1111111254fb6c44bAC0beD2854e76F90643097d', func: OneInchSwapFunc },
-      { address: '0x1111111254fb6c44bAC0beD2854e76F90643097d', func: OneInchClipperSwapFunc },
-      { address: '0x1111111254fb6c44bAC0beD2854e76F90643097d', func: OneInchUnoswapSwapFunc },
-      { address: '0x1111111254fb6c44bAC0beD2854e76F90643097d', func: OneInchUnoswapV3SwapFunc },
+      ...getSupportedOneInchFuncs('0x1111111254fb6c44bAC0beD2854e76F90643097d'),
       ...getSupportedCurvePools(137)
     ],
     codecs: [
@@ -106,9 +88,8 @@ export const deploymentConfigs: IConfig = {
       getMetaPoolCodecConfig(137)
     ],
     externalSwapDex: ['0x1111111254fb6c44bac0bed2854e76f90643097d'], // 1inch
-    transferSwapper: process.env.TRANSFER_SWAPPER_137, //set with the actual addr after TransferSwapper is deployed
-    anyswapRouters: (process.env.ANYSWAP_ROUTER_137 ?? '').split(','),
-    stargateRouters: (process.env.STARGATE_ROUTER_137 ?? '').split(',')
+    anyswapRouters: ['0x4f3Aff3A747fCADe12598081e80c6605A8be192F', '0x6ff0609046a38d76bd40c5863b4d1a2dce687f73'],
+    stargateRouters: ['0x45A01E4e04F14f7A4a6702c74187c5F6222033cd']
   },
 
   // Fantom
@@ -118,18 +99,34 @@ export const deploymentConfigs: IConfig = {
     supportedDex: [
       { address: '0xF491e7B69E4244ad4002BC14e878a34207E38c29', func: UniswapV2SwapExactTokensForTokensCodec.func }, // Spookyswap: UniswapV2Router02
       { address: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506', func: UniswapV2SwapExactTokensForTokensCodec.func }, // SushiSwap: SushiSwapRouter
-      // 1inch address and funcs
-      { address: '0x1111111254fb6c44bAC0beD2854e76F90643097d', func: OneInchSwapFunc },
-      { address: '0x1111111254fb6c44bAC0beD2854e76F90643097d', func: OneInchClipperSwapFunc },
-      { address: '0x1111111254fb6c44bAC0beD2854e76F90643097d', func: OneInchUnoswapSwapFunc },
-      { address: '0x1111111254fb6c44bAC0beD2854e76F90643097d', func: OneInchUnoswapV3SwapFunc },
+      ...getSupportedOneInchFuncs('0x1111111254fb6c44bAC0beD2854e76F90643097d'),
       ...getSupportedCurvePools(250)
     ],
     codecs: [UniswapV2SwapExactTokensForTokensCodec, CurvePoolCodec, getMetaPoolCodecConfig(250)],
     externalSwapDex: ['0x1111111254fb6c44bac0bed2854e76f90643097d'], // 1inch
-    transferSwapper: process.env.TRANSFER_SWAPPER_250, //set with the actual addr after TransferSwapper is deployed
-    anyswapRouters: (process.env.ANYSWAP_ROUTER_250 ?? '').split(','),
-    stargateRouters: (process.env.STARGATE_ROUTER_250 ?? '').split(',')
+    anyswapRouters: ['0x1ccca1ce62c62f7be95d4a67722a8fdbed6eecb4', '0xb576c9403f39829565bd6051695e2ac7ecf850e2'],
+    stargateRouters: ['0xAf5191B0De278C7286d6C7CC6ab6BB8A73bA2Cd6']
+  },
+
+  // Arbitrum
+  42161: {
+    nativeWrap: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
+    messageBus: process.env.MESSAGE_BUS_42161,
+    supportedDex: [
+      { address: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506', func: UniswapV2SwapExactTokensForTokensCodec.func }, // Sushiswap: SushiSwapRouter
+      { address: '0xE592427A0AEce92De3Edee1F18E0157C05861564', func: UniswapV3ExactInputCodec.func }, // UniswapV3: SwapRouter
+      ...getSupportedOneInchFuncs('0x1111111254fb6c44bAC0beD2854e76F90643097d'),
+      ...getSupportedCurvePools(42161)
+    ],
+    codecs: [
+      UniswapV2SwapExactTokensForTokensCodec,
+      UniswapV3ExactInputCodec,
+      CurvePoolCodec,
+      getMetaPoolCodecConfig(42161)
+    ],
+    externalSwapDex: ['0x1111111254fb6c44bac0bed2854e76f90643097d'], // 1inch
+    anyswapRouters: ['0xc931f61b1534eb21d8c11b24f3f5ab2471d4ab50', '0x0cae51e1032e8461f4806e26332c030e34de3adb'],
+    stargateRouters: ['0x53Bf833A5d6c4ddA888F69c22C88C9f356a41614', '0xbf22f0f184bCcbeA268dF387a49fF5238dD23E40']
   },
 
   // Avalanche
@@ -140,11 +137,7 @@ export const deploymentConfigs: IConfig = {
       { address: '0x60aE616a2155Ee3d9A68541Ba4544862310933d4', func: UniswapV2SwapExactTokensForTokensCodec.func }, // TraderJoe: JoeRouter02
       { address: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506', func: UniswapV2SwapExactTokensForTokensCodec.func }, // SushiSwap: SushiSwapRouter
       { address: '0x73256EC7575D999C360c1EeC118ECbEFd8DA7D12', func: PlatypusRouter01Codec.func }, // Platypus: PlatypusRouter01
-      // 1inch address and funcs
-      { address: '0x1111111254fb6c44bAC0beD2854e76F90643097d', func: OneInchSwapFunc },
-      { address: '0x1111111254fb6c44bAC0beD2854e76F90643097d', func: OneInchClipperSwapFunc },
-      { address: '0x1111111254fb6c44bAC0beD2854e76F90643097d', func: OneInchUnoswapSwapFunc },
-      { address: '0x1111111254fb6c44bAC0beD2854e76F90643097d', func: OneInchUnoswapV3SwapFunc },
+      ...getSupportedOneInchFuncs('0x1111111254fb6c44bAC0beD2854e76F90643097d'),
       ...getSupportedCurvePools(43114)
     ],
     codecs: [
@@ -155,34 +148,7 @@ export const deploymentConfigs: IConfig = {
       getMetaPoolCodecConfig(43114)
     ],
     externalSwapDex: ['0x1111111254fb6c44bac0bed2854e76f90643097d'], // 1inch
-    transferSwapper: process.env.TRANSFER_SWAPPER_43114, //set with the actual addr after TransferSwapper is deployed
-    anyswapRouters: (process.env.ANYSWAP_ROUTER_43114 ?? '').split(','),
-    stargateRouters: (process.env.STARGATE_ROUTER_43114 ?? '').split(',')
-  },
-
-  // Arbitrum
-  42161: {
-    nativeWrap: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
-    messageBus: process.env.MESSAGE_BUS_42161,
-    supportedDex: [
-      { address: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506', func: UniswapV2SwapExactTokensForTokensCodec.func }, // Sushiswap: SushiSwapRouter
-      { address: '0xE592427A0AEce92De3Edee1F18E0157C05861564', func: UniswapV3ExactInputCodec.func }, // UniswapV3: SwapRouter
-      // 1inch address and funcs
-      { address: '0x1111111254fb6c44bAC0beD2854e76F90643097d', func: OneInchSwapFunc },
-      { address: '0x1111111254fb6c44bAC0beD2854e76F90643097d', func: OneInchClipperSwapFunc },
-      { address: '0x1111111254fb6c44bAC0beD2854e76F90643097d', func: OneInchUnoswapSwapFunc },
-      { address: '0x1111111254fb6c44bAC0beD2854e76F90643097d', func: OneInchUnoswapV3SwapFunc },
-      ...getSupportedCurvePools(42161)
-    ],
-    codecs: [
-      UniswapV2SwapExactTokensForTokensCodec,
-      UniswapV3ExactInputCodec,
-      CurvePoolCodec,
-      getMetaPoolCodecConfig(42161)
-    ],
-    externalSwapDex: ['0x1111111254fb6c44bac0bed2854e76f90643097d'], // 1inch
-    transferSwapper: process.env.TRANSFER_SWAPPER_42161, //set with the actual addr after TransferSwapper is deployed
-    anyswapRouters: (process.env.ANYSWAP_ROUTER_42161 ?? '').split(','),
-    stargateRouters: (process.env.STARGATE_ROUTER_42161 ?? '').split(',')
+    anyswapRouters: ['0x42e6Af231bA8eE39f1bf185d452061283bfB324b', '0xe5cf1558a1470cb5c166c2e8651ed0f3c5fb8f42'],
+    stargateRouters: ['0xB0D502E938ed5f4df2E681fE6E419ff29631d62b', '0xB49c4e680174E331CB0A7fF3Ab58afC9738d5F8b']
   }
 };
