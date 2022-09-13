99630d134a532353ee1bb34d992d80c5f7980a0b
lionelho <lionel.ho@outlook.com>
2022-09-13 21:03:08 +0800

    support non-underlying anyToken

--- contracts/interfaces/IBridgeAnyswap.sol
+++ contracts/interfaces/IBridgeAnyswap.sol
@@ -10,3 +10,7 @@ interface IBridgeAnyswap {
     // Swaps `amount` `token` from this chain to `toChainID` chain with recipient `to` by minting with `underlying`
     function anySwapOutUnderlying(address token, address to, uint amount, uint toChainID) external;
 }
+
+interface IUnderlying {
+    function underlying() external view returns (address);
+}
