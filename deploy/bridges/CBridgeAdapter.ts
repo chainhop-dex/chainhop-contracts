3fd3289f1273643dd5b50c4115e6b32a95da9aa2
padoriku <patrickmao@live.com>
2022-09-13 19:20:10 +0800

    refine deployment script

--- deploy/bridges/CBridgeAdapter.ts
+++ deploy/bridges/CBridgeAdapter.ts
@@ -1,8 +1,10 @@
 import * as dotenv from 'dotenv';
+import { ethers } from 'hardhat';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import { isTestnet, testnetDeploymentConfigs } from '../configs/testnetConfig';
+import { CBridgeAdapter__factory } from '../../typechain';
 import { deploymentConfigs } from '../configs/config';
+import { isTestnet, testnetDeploymentConfigs } from '../configs/testnetConfig';
 
 dotenv.config();
 
@@ -13,15 +15,21 @@ const deployFunc: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const chainId = parseInt(await hre.getChainId(), 10);
   const configs = isTestnet(chainId) ? testnetDeploymentConfigs : deploymentConfigs;
   const config = configs[chainId];
+  const xswap = await deployments.get('TransferSwapper');
+  console.log('TransferSwapper', xswap.address);
 
-  await deploy('CBridgeAdapter', {
+  const result = await deploy('CBridgeAdapter', {
     from: deployer,
     log: true,
-    args: [
-      config.transferSwapper,
-      config.messageBus
-    ]
+    args: [xswap.address, config.messageBus]
   });
+
+  const factory = await ethers.getContractFactory<CBridgeAdapter__factory>('CBridgeAdapter');
+  const adapter = factory.attach(result.address);
+  const tx = await adapter.connect(deployer).updateMainContract(xswap.address);
+  console.log('updateMainContract for cbridge adapter: tx', tx.hash);
+  await tx.wait(5);
+  console.log(`updateMainContract for cbridge adapter: tx ${tx.hash} done`);
 };
 
 deployFunc.tags = ['CBridgeAdapter'];
