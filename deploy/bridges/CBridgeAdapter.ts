e94300377cba57395a612f3b441efd38487fc653
padoriku <patrickmao@live.com>
2022-09-14 14:00:42 +0800

    complete cbridge adapter deploy script

--- deploy/bridges/CBridgeAdapter.ts
+++ deploy/bridges/CBridgeAdapter.ts
@@ -2,8 +2,9 @@ import * as dotenv from 'dotenv';
 import { ethers } from 'hardhat';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import { CBridgeAdapter__factory } from '../../typechain';
+import { CBridgeAdapter__factory, TransferSwapper__factory } from '../../typechain';
 import { deploymentConfigs } from '../configs/config';
+import { verify } from '../configs/functions';
 import { isTestnet, testnetDeploymentConfigs } from '../configs/testnetConfig';
 
 dotenv.config();
@@ -17,19 +18,32 @@ const deployFunc: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const config = configs[chainId];
   const xswap = await deployments.get('TransferSwapper');
   console.log('TransferSwapper', xswap.address);
+  console.log('MessageBus', config.messageBus);
 
+  const args = [xswap.address, config.messageBus];
   const result = await deploy('CBridgeAdapter', {
     from: deployer,
     log: true,
-    args: [xswap.address, config.messageBus]
+    args: args
   });
+
+  const deployerSigner = await ethers.getSigner(deployer);
+
+  const xswapFactory = await ethers.getContractFactory<TransferSwapper__factory>('TransferSwapper');
+  const main = xswapFactory.attach(xswap.address);
+  let tx = await main.connect(deployerSigner).setSupportedBridges(['cbridge'], [result.address]);
+  console.log('setSupportedBridges: tx', tx.hash);
+  await tx.wait(5);
+  console.log(`setSupportedBridges: tx ${tx.hash} done`);
 
   const factory = await ethers.getContractFactory<CBridgeAdapter__factory>('CBridgeAdapter');
   const adapter = factory.attach(result.address);
-  const tx = await adapter.connect(deployer).updateMainContract(xswap.address);
+  tx = await adapter.connect(deployerSigner).updateMainContract(xswap.address);
   console.log('updateMainContract for cbridge adapter: tx', tx.hash);
   await tx.wait(5);
   console.log(`updateMainContract for cbridge adapter: tx ${tx.hash} done`);
+
+  await verify(hre, result, args);
 };
 
 deployFunc.tags = ['CBridgeAdapter'];
