34c1b2489a8444174d29aa6d638674cf63c20b49
padoriku <patrickmao@live.com>
2022-09-14 15:27:09 +0800

    update anyswap adapter deploy script

--- deploy/bridges/AnyswapAdapter.ts
+++ deploy/bridges/AnyswapAdapter.ts
@@ -1,8 +1,12 @@
 import * as dotenv from 'dotenv';
+import { ethers } from 'hardhat';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
-import { isTestnet, testnetDeploymentConfigs } from '../configs/testnetConfig';
+import { TransferSwapper__factory } from '../../typechain';
 import { deploymentConfigs } from '../configs/config';
+import { verify } from '../configs/functions';
+import { isTestnet, testnetDeploymentConfigs } from '../configs/testnetConfig';
+import { AnyswapAdapter__factory } from './../../typechain/factories/AnyswapAdapter__factory';
 
 dotenv.config();
 
@@ -13,15 +17,33 @@ const deployFunc: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const chainId = parseInt(await hre.getChainId(), 10);
   const configs = isTestnet(chainId) ? testnetDeploymentConfigs : deploymentConfigs;
   const config = configs[chainId];
+  const xswap = await deployments.get('TransferSwapper');
+  console.log('TransferSwapper', xswap.address);
 
-  await deploy('AnyswapAdapter', {
+  const args = [xswap.address, config.anyswapRouters];
+  const result = await deploy('AnyswapAdapter', {
     from: deployer,
     log: true,
-    args: [
-      config.transferSwapper,
-      config.anyswapRouters
-    ]
+    args: args
   });
+
+  const deployerSigner = await ethers.getSigner(deployer);
+
+  const xswapFactory = await ethers.getContractFactory<TransferSwapper__factory>('TransferSwapper');
+  const main = xswapFactory.attach(xswap.address);
+  let tx = await main.connect(deployerSigner).setSupportedBridges(['anyswap'], [result.address]);
+  console.log('setSupportedBridges: tx', tx.hash);
+  await tx.wait(5);
+  console.log(`setSupportedBridges: tx ${tx.hash} done`);
+
+  const factory = await ethers.getContractFactory<AnyswapAdapter__factory>('AnyswapAdapter');
+  const adapter = factory.attach(result.address);
+  tx = await adapter.connect(deployerSigner).updateMainContract(xswap.address);
+  console.log('updateMainContract for anyswap adapter: tx', tx.hash);
+  await tx.wait(5);
+  console.log(`updateMainContract for anyswap adapter: tx ${tx.hash} done`);
+
+  await verify(hre, result, args);
 };
 
 deployFunc.tags = ['AnyswapAdapter'];
