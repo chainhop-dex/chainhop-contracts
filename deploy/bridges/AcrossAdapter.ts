eb5f9b77f6f6131122b11db7057099013c8cd479
padoriku <patrickmao@live.com>
2022-09-16 15:22:13 +0800

    refine

---
+++ deploy/bridges/AcrossAdapter.ts
@@ -0,0 +1,43 @@
+import * as dotenv from 'dotenv';
+import { ethers } from 'hardhat';
+import { DeployFunction } from 'hardhat-deploy/types';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { TransferSwapper__factory } from '../../typechain';
+import { deploymentConfigs } from '../configs/config';
+import { verify } from '../configs/functions';
+import { isTestnet, testnetDeploymentConfigs } from '../configs/testnetConfig';
+
+dotenv.config();
+
+const deployFunc: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = parseInt(await hre.getChainId(), 10);
+  const configs = isTestnet(chainId) ? testnetDeploymentConfigs : deploymentConfigs;
+  const config = configs[chainId];
+  const xswap = await deployments.get('TransferSwapper');
+  console.log('TransferSwapper', xswap.address);
+
+  const args = [config.acrossSpokePool];
+  const result = await deploy('AcrossAdapter', {
+    from: deployer,
+    log: true,
+    args: args
+  });
+
+  const deployerSigner = await ethers.getSigner(deployer);
+
+  const xswapFactory = await ethers.getContractFactory<TransferSwapper__factory>('TransferSwapper');
+  const main = xswapFactory.attach(xswap.address);
+  const tx = await main.connect(deployerSigner).setSupportedBridges(['across'], [result.address]);
+  console.log('setSupportedBridges: tx', tx.hash);
+  await tx.wait(5);
+  console.log(`setSupportedBridges: tx ${tx.hash} done`);
+
+  await verify(hre, result, args);
+};
+
+deployFunc.tags = ['AcrossAdapter'];
+deployFunc.dependencies = [];
+export default deployFunc;
