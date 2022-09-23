7184ca6e40aa2826a25b1e0bdeff5fefb7ace741
padoriku <patrickmao@live.com>
2022-09-23 16:31:20 +0800

    support deposit native for hyphen

--- contracts/bridges/HyphenAdapter.sol
+++ contracts/bridges/HyphenAdapter.sol
@@ -8,15 +8,18 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "../interfaces/IBridgeAdapter.sol";
 import "../interfaces/IHyphenLiquidityPool.sol";
+import "../interfaces/IWETH.sol";
+import "../lib/NativeWrap.sol";
 
-contract HyphenAdapter is IBridgeAdapter, Ownable {
+contract HyphenAdapter is IBridgeAdapter, NativeWrap {
     using SafeERC20 for IERC20;
 
     address public pool;
 
     event PoolUpdated(address pool);
 
-    constructor(address _pool) {
+    constructor(address _pool, address _nativeWrap) NativeWrap(_nativeWrap) {
+        require(_pool != address(0), "zero address");
         pool = _pool;
     }
 
@@ -29,8 +32,14 @@ contract HyphenAdapter is IBridgeAdapter, Ownable {
         bytes memory // _requestMessage
     ) external payable returns (bytes memory bridgeResp) {
         IERC20(_token).safeTransferFrom(msg.sender, address(this), _amount);
-        IERC20(_token).approve(pool, _amount);
-        IHyphenLiquidityPool(pool).depositErc20(_dstChainId, _token, _receiver, _amount, "chainhop");
+        if (_token == nativeWrap) {
+            // depositErc20 doesn't work for WETH, so we have to convert it back to native first
+            IWETH(nativeWrap).withdraw(_amount);
+            IHyphenLiquidityPool(pool).depositNative{value: _amount}(_receiver, _dstChainId, "chainhop");
+        } else {
+            IERC20(_token).approve(pool, _amount);
+            IHyphenLiquidityPool(pool).depositErc20(_dstChainId, _token, _receiver, _amount, "chainhop");
+        }
         // hyphen uses src tx hash to track history so bridgeResp is not needed. returning empty
         return bridgeResp;
     }
@@ -39,4 +48,6 @@ contract HyphenAdapter is IBridgeAdapter, Ownable {
         pool = _pool;
         emit PoolUpdated(_pool);
     }
+
+    receive() external payable {}
 }
