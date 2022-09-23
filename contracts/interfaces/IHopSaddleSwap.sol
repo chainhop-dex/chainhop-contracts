61084a0c5a5ac678572cc603ad55f48d7c74ba37
padoriku <patrickmao@live.com>
2022-09-23 13:59:20 +0800

    add hop interface

---
+++ contracts/interfaces/IHopSaddleSwap.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.15;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+interface ISaddleSwap {
+    // pool data view functions
+    function getA() external view returns (uint256);
+
+    function getToken(uint8 index) external view returns (IERC20);
+
+    function getTokenIndex(address tokenAddress) external view returns (uint8);
+
+    function getTokenBalance(uint8 index) external view returns (uint256);
+
+    function getVirtualPrice() external view returns (uint256);
+
+    function isGuarded() external view returns (bool);
+
+    // min return calculation functions
+    function calculateSwap(
+        uint8 tokenIndexFrom,
+        uint8 tokenIndexTo,
+        uint256 dx
+    ) external view returns (uint256);
+
+    function calculateTokenAmount(uint256[] calldata amounts, bool deposit) external view returns (uint256);
+
+    function swap(
+        uint8 tokenIndexFrom,
+        uint8 tokenIndexTo,
+        uint256 dx,
+        uint256 minDy,
+        uint256 deadline
+    ) external returns (uint256);
+}
