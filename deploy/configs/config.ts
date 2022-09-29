51dc87c03af67807b83400b1ac2aa69c4ae5d22c
padoriku <patrickmao@live.com>
2022-09-29 20:55:23 +0800

    implement hop adapter and deploy scripts

--- deploy/configs/config.ts
+++ deploy/configs/config.ts
@@ -21,6 +21,7 @@ export const deploymentConfigs: IConfig = {
   feeCollector: process.env.FEE_COLLECTOR,
 
   1: {
+    isL1: true,
     nativeWrap: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
     messageBus: process.env.MESSAGE_BUS_1,
     supportedDex: [
@@ -45,11 +46,21 @@ export const deploymentConfigs: IConfig = {
     ],
     stargateRouters: ['0x8731d54E9D02c286767d56ac03e8037C07e01e98', '0x150f94B44927F078737562f0fcF3C95c01Cc2376'],
     acrossSpokePool: '0x4D9079Bb4165aeb4084c526a32695dCfd2F77381',
-    hyphenLiquidityPool: '0x2A5c2568b10A0E826BfA892Cf21BA7218310180b'
+    hyphenLiquidityPool: '0x2A5c2568b10A0E826BfA892Cf21BA7218310180b',
+    hopBridges: {
+      '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48': '0x3666f603Cc164936C1b87e207F36BEBa4AC5f18a', // USDC
+      '0xdAC17F958D2ee523a2206206994597C13D831ec7': '0x3E4a3a4796d16c0Cd582C382691998f7c06420B6', // USDT
+      '0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0': '0x22B1Cbb8D98a01a3B71D034BB899775A76Eb1cc2', // WMATIC
+      '0x6B175474E89094C44Da98b954EedeAC495271d0F': '0x3d4Cc8A61c7528Fd86C55cfe061a78dCBA48EDd1', // DAI
+      '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2': '0xb8901acB165ed027E32754E0FFe830802919727f', // WETH
+      '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599': '0xb98454270065A31D71Bf635F6F7Ee6A518dFb849', // WBTC
+      '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f': '0x893246FACF345c99e4235E5A7bbEE7404c988b96' // SNX
+    }
   },
 
   // Optimism
   10: {
+    isL1: false,
     nativeWrap: '0x4200000000000000000000000000000000000006',
     messageBus: process.env.MESSAGE_BUS_10,
     supportedDex: [
@@ -61,11 +72,20 @@ export const deploymentConfigs: IConfig = {
     anyswapRouters: ['0xDC42728B0eA910349ed3c6e1c9Dc06b5FB591f98', '0x80a16016cc4a2e6a2caca8a4a498b1699ff0f844'],
     stargateRouters: ['0xB0D502E938ed5f4df2E681fE6E419ff29631d62b', '0xB49c4e680174E331CB0A7fF3Ab58afC9738d5F8b'],
     acrossSpokePool: '0xa420b2d1c0841415A695b81E5B867BCD07Dff8C9',
-    hyphenLiquidityPool: '0x856cb5c3cbbe9e2e21293a644aa1f9363cee11e8'
+    hyphenLiquidityPool: '0x856cb5c3cbbe9e2e21293a644aa1f9363cee11e8',
+    hopBridges: {
+      '0x7F5c764cBc14f9669B88837ca1490cCa17c31607': '0x2ad09850b0CA4c7c1B33f5AcD6cBAbCaB5d6e796', // USDC
+      '0x94b008aA00579c1307B0EF2c499aD98a8ce58e58': '0x7D269D3E0d61A05a0bA976b7DBF8805bF844AF3F', // USDT
+      '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1': '0xb3C68a491608952Cb1257FC9909a537a0173b63B', // DAI
+      '0x4200000000000000000000000000000000000006': '0x86cA30bEF97fB651b8d866D45503684b90cb3312', // WETH
+      '0x68f180fcCe6836688e9084f035309E29Bf0A2095': '0x2A11a98e2fCF4674F30934B5166645fE6CA35F56', // WBTC
+      '0x8700dAec35aF8Ff88c16BdF0418774CB3D7599B4': '0xf11EBB94EC986EA891Aec29cfF151345C83b33Ec' // SNX
+    }
   },
 
   // BSC
   56: {
+    isL1: true,
     nativeWrap: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c',
     messageBus: process.env.MESSAGE_BUS_56,
     supportedDex: [
@@ -87,6 +107,7 @@ export const deploymentConfigs: IConfig = {
 
   // Polygon
   137: {
+    isL1: false,
     nativeWrap: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
     messageBus: process.env.MESSAGE_BUS_137,
     supportedDex: [
@@ -111,11 +132,20 @@ export const deploymentConfigs: IConfig = {
     ],
     stargateRouters: ['0x45A01E4e04F14f7A4a6702c74187c5F6222033cd'],
     acrossSpokePool: '0x69B5c72837769eF1e7C164Abc6515DcFf217F920',
-    hyphenLiquidityPool: '0x2A5c2568b10A0E826BfA892Cf21BA7218310180b'
+    hyphenLiquidityPool: '0x2A5c2568b10A0E826BfA892Cf21BA7218310180b',
+    hopBridges: {
+      '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174': '0x76b22b8C1079A44F1211D867D68b1eda76a635A7', // USDC
+      '0xc2132D05D31c914a87C6611C10748AEb04B58e8F': '0x8741Ba6225A6BF91f9D73531A98A89807857a2B3', // USDT
+      '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270': '0x884d1Aa15F9957E1aEAA86a82a72e49Bc2bfCbe3', // WMATIC
+      '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063': '0x28529fec439cfF6d7D1D5917e956dEE62Cd3BE5c', // DAI
+      '0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619': '0xc315239cFb05F1E130E7E28E603CEa4C014c57f0', // WETH
+      '0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6': '0xCd1d7AEfA8055e020db0d0e98bbF3FeD1A16aad6' // WBTC
+    }
   },
 
   // Fantom
   250: {
+    isL1: true,
     nativeWrap: '0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83',
     messageBus: process.env.MESSAGE_BUS_250,
     supportedDex: [
@@ -137,6 +167,7 @@ export const deploymentConfigs: IConfig = {
 
   // Arbitrum
   42161: {
+    isL1: false,
     nativeWrap: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
     messageBus: process.env.MESSAGE_BUS_42161,
     supportedDex: [
@@ -160,11 +191,19 @@ export const deploymentConfigs: IConfig = {
     ],
     stargateRouters: ['0x53Bf833A5d6c4ddA888F69c22C88C9f356a41614', '0xbf22f0f184bCcbeA268dF387a49fF5238dD23E40'],
     acrossSpokePool: '0xB88690461dDbaB6f04Dfad7df66B7725942FEb9C',
-    hyphenLiquidityPool: '0x856cb5c3cbbe9e2e21293a644aa1f9363cee11e8'
+    hyphenLiquidityPool: '0x856cb5c3cbbe9e2e21293a644aa1f9363cee11e8',
+    hopBridges: {
+      '0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8': '0xe22D2beDb3Eca35E6397e0C6D62857094aA26F52', // USDC
+      '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9': '0xCB0a4177E0A60247C0ad18Be87f8eDfF6DD30283', // USDT
+      '0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1': '0xe7F40BF16AB09f4a6906Ac2CAA4094aD2dA48Cc2', // DAI
+      '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1': '0x33ceb27b39d2Bb7D2e61F7564d3Df29344020417', // WETH
+      '0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f': '0xC08055b634D43F2176d721E26A3428D3b7E7DdB5' // WBTC
+    }
   },
 
   // Avalanche
   43114: {
+    isL1: true,
     nativeWrap: '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7',
     messageBus: process.env.MESSAGE_BUS_43114,
     supportedDex: [
